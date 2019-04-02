81d4be17a4cdd769220f05f07d35c460676b673f
Neo <echiang73@gmail.com>
2019-04-01 20:42:49 -0700

    Incorporated working version of BandsInTown using axios san moment.js

--- liri.js
+++ liri.js
@@ -9,9 +9,9 @@ var command = process.argv[2];
 var userInput = process.argv.slice(3).join(" ");
 
 switch (command) {
-    // case "concert-this":
-    //     concertThis(userInput);
-    //     break;
+    case "concert-this":
+        concertThis(userInput);
+        break;
     case "spotify-this-song":
         spotifyThis(userInput);
         break;
@@ -25,7 +25,40 @@ switch (command) {
         console.log("Try again and enter a command");
 };
 
-
+function concertThis(userInput) {
+    // We then run the request with axios module on a URL with a JSON
+    axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            // Then we print out the responses
+            // console.log(response.data[0]);
+           for (var i=0; i<response.data.length; i++) {
+            // Name of the venue
+            console.log("Venue #" + [i+1] + ": " + response.data[i].venue.name);
+            // Venue location
+            console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
+            // Date of the Event (use moment to format this as "MM/DD/YYYY")
+           }
+          
+            
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
 
 
 function spotifyThis(userInput) {
