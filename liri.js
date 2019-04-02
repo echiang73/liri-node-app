2cd8c0a839b6bd92ec4203af6ba939ff1ba11fa7
Neo <echiang73@gmail.com>
2019-04-01 20:47:43 -0700

    npm install moment

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ var keys = require("./keys.js");
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var fs = require('fs');
-// var moment = require("moment");
+var moment = require("moment");
 
 var command = process.argv[2];
 var userInput = process.argv.slice(3).join(" ");
